a4b9ec1aa77cfbd42df8f9defa1c8aaa1dd18195
Misha Ponizil <misha.ponizil@gmail.com>
2011-08-20 21:16:34 -0400

    testing fb stuff

--- jpegcam/upload.php
+++ jpegcam/upload.php
@@ -19,10 +19,10 @@ function curl_url($url) {
 
 $filename = date('YmdHis') . '.jpg';
 $result = file_put_contents( $filename, file_get_contents('php://input') );
-if (!$result) {
-	print "ERROR: Failed to write data to $filename, check permissions\n";
-	exit();
-}
+// if (!$result) {
+//  print "ERROR: Failed to write data to $filename, check permissions\n";
+//  exit();
+// }
 $image_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/' . $filename;
 
 
