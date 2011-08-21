9a33e9778722bcfb16c652770121bd9cd85bda51
Misha Ponizil <misha.ponizil@gmail.com>
2011-08-20 21:35:53 -0400

    forgot curl url

--- face_magic.php
+++ face_magic.php
@@ -1,5 +1,19 @@
 <?php
 
+define(FACE_API_KEY,"9d7f7b0e135972820852c9665c46a352");
+define(FACE_API_SECRET,"3496ddb11098381389cda9ea0908ce5e");
+
+function curl_url($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch,CURLOPT_URL,$url);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+
 $image_url = $_POST['image_url'];
 $fb_user_id = $_POST['fb_user_id'];
 $fb_oauth_token = $_POST['fb_oauth_token'];
--- jpegcam/upload.php
+++ jpegcam/upload.php
@@ -1,19 +1,5 @@
 <?php
 
-define(FACE_API_KEY,"9d7f7b0e135972820852c9665c46a352");
-define(FACE_API_SECRET,"3496ddb11098381389cda9ea0908ce5e");
-
-function curl_url($url) {
-	$ch = curl_init();
-	$timeout = 5;
-	curl_setopt($ch,CURLOPT_URL,$url);
-	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-	$data = curl_exec($ch);
-	curl_close($ch);
-	return $data;
-}
-
 $filename = date('YmdHis') . '.jpg';
 $result = file_put_contents( $filename, file_get_contents('php://input') );
 if (!$result) {
