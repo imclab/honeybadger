c20f1ab0b58768cd921ca0f84b448a3fb19cd492
Misha Ponizil <misha.ponizil@gmail.com>
2011-08-20 20:02:21 -0400

    jpegcam working

---
+++ jpegcam/upload.php
@@ -0,0 +1,16 @@
+<?php
+
+/* JPEGCam Test Script */
+/* Receives JPEG webcam submission and saves to local file. */
+/* Make sure your directory has permission to write files as your web server user! */
+
+$filename = date('YmdHis') . '.jpg';
+
+$result = file_put_contents( $filename, file_get_contents('php://input') );
+
+if (!$result) {
+	print "ERROR: Failed to write data to $filename, check permissions\n";
+	exit();
+}
+
+?>
